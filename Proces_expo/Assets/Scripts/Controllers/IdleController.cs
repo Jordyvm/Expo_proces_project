997fb206dfb46f5bc2e76ff676b7d930139919e4
Jarne Cooijmans <jarnec@hotmail.com>
2018-06-11 13:45:10 +0200

    begin controllers gemaakt

---
+++ Proces_expo/Assets/Scripts/Controllers/IdleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleController : MonoBehaviour
+{
+    [Header("Time Variables")]
+    [Tooltip("The current time in the scene")]
+    public float currentTime;
+    [Tooltip("The delay till the scene is reloaded")]
+    public float IdleDelay;
+    float timeLeft;
+    MenuController menuController;
+
+    private void Awake()
+    {
+        menuController = GameObject.Find("GameController").GetComponent<MenuController>();
+        timeLeft = IdleDelay;
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(timeLeft < currentTime){
+            print("delay is over");
+            timeLeft = timeLeft + IdleDelay;
+            menuController._idleScene();
+        }
+    }
+}
---
+++ Proces_expo/Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    /* Functions used to navigate between the diffrent scenes */
+    #region Menu functions
+    public void _idleScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(0).name);
+    }
+    public void _Gary()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(1).name);
+    }
+
+    public void _Oby()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(2).name);
+    }
+    public void _Dotto()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(3).name);
+    }
+    public void _Eind()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(4).name);
+    }
+    #endregion
+}
