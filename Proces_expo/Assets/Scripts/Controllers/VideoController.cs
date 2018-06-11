9c31eea4eaede388bc11cfc3e748293cc2c79089
Jarne Cooijmans <jarnec@hotmail.com>
2018-06-11 14:26:18 +0200

    goedeversie

---
+++ Proces_expo/Assets/Scripts/Controllers/VideoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+    public VideoPlayer[] myVideos;
+    public bool[] allowedToPlay;
+
+    public int videoCounter = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        foreach(VideoPlayer videos in myVideos){
+        }
+            
+	}
+}
